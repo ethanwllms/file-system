907e6ebb830fc9d8a73aa0ae64d701c7eaf6b3cb
Ethan Williams <ethanwilliams@Ethans-Mac-Studio.local>
2025-01-17 18:29:50 -0800

    troubleshooting auto_test workflow

--- fs-client/setup.sh
+++ fs-client/setup.sh
@@ -3,3 +3,6 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+yes Y | pip uninstall websocket 
+yes Y | pip uninstall websocket-client 
+pip install websocket-client
